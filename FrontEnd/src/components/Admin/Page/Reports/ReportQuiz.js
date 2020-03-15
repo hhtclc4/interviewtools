a77b8e8894d6f9b8321d70af0c11cf6e1083ace8
hoangclc4 <hoangclc4@gmail.com>
2020-03-15 14:39:32 +0700

    ??

--- FrontEnd/src/components/Admin/Page/Reports/ReportQuiz.js
+++ FrontEnd/src/components/Admin/Page/Reports/ReportQuiz.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import './Reports.scss'
+import React from "react";
+import "./Reports.scss";
 import { withRouter } from "react-router-dom";
 
 class ReportQuiz extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            accuracy: 2,
-        }
-    }
-    render() {
-        let { accuracy } = this.state;
-        let accuracyColor = () => {
-            if (accuracy > 85)
-                return "#7AD18E";
-            if (accuracy > 50)
-                return "#FDC954";
-            return "#EC0B43";
-        }
-        let { history } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      accuracy: 2
+    };
+  }
+  render() {
+    let { accuracy } = this.state;
+    let accuracyColor = () => {
+      if (accuracy > 85) return "#7AD18E";
+      if (accuracy > 50) return "#FDC954";
+      return "#EC0B43";
+    };
+    let { history } = this.props;
 
-        return (
-            <div className="report-quiz-container container"
-                onClick={() => {
-                    history.push(`/admin/reports/report_detail`);
-                }}
+    return (
+      <div
+        className="report-quiz-container container"
+        onClick={() => {
+          history.push(`/admin/reports/report_detail`);
+        }}
+      >
+        <div className="rq-row row">
+          <div className="rq-type col-sm">Live</div>
+          <div className="rq-name col-sm">Quiz name</div>
+          <div className="rq-players col-sm">Count</div>
+          <div className="rq-accuracy col-sm">
+            <span
+              className="rounded"
+              style={{
+                backgroundColor: accuracyColor(),
+                padding: "8px",
+                color: "white"
+              }}
             >
-                <div className="rq-row row">
-                    <div className="rq-type col-sm">
-                        Type name
-                    </div>
-                    <div className="rq-name col-sm">
-                        Quiz name
-                    </div>
-                    <div className="rq-players col-sm">
-                        Count
-                    </div>
-                    <div className="rq-accuracy col-sm" >
-                        <span className="rounded"
-                            style={{ backgroundColor: accuracyColor(), padding: '8px', color: 'white' }}>{accuracy}%</span>
-                    </div>
-                    <div className="rq-option col-sm"></div>
-                </div>
-            </div>
-        );
-    }
+              {accuracy}%
+            </span>
+          </div>
+          <div className="rq-option col-sm"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(ReportQuiz);
