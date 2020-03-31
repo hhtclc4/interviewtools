61dd6a7ef2b2347ef9d562c29ffd60ce9186b3a4
Minh Tri <handcock.dk@gmail.com>
2020-04-01 00:07:06 +0700

    add HR

--- FrontEnd/src/components/HR/HR.js
+++ FrontEnd/src/components/HR/HR.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './HR.scss'
 import HRNav from './Nav/Nav'
+import CampaignOverview from './CampaignOverview/CampaignOverview'
 
 class HRstaff extends React.Component {
     constructor(props) {
@@ -12,23 +13,30 @@ class HRstaff extends React.Component {
             <div className="hr-page-container">
                 <HRNav />
                 <div className="hr-page-grid">
-                    <div className="row">
+                    <div className="row" style={{ height: '100%' }}>
                         <div className="col-sm-1"></div>
-                        <div className="col-xl-9">
-                            <div className="hr-created-list  d-flex flex-column ">
-                                <div className="hr-campaign-container">
-                                </div>
-                                <div className="hr-campaign-container">
-                                </div>
+                        <div className="col-lg-10 hr-manage-container">
+                            <div className="hr-manage-header d-flex flex-row justify-content-between">
+                                <p className="hr-campaign-count align-self-center">
+                                    All campaigns (11)
+                                </p>
+                                <button className="hr-create-campaign-btn align-self-center">Create new campaign</button>
                             </div>
-                            <div className="hr-create-campaign">
-                                <button className="create-campaign-btn">Create a new campaign</button>
+
+                            <div className="hr-manage-body d-flex flex-row">
+                                <div className="hr-created-list  d-flex flex-column mt-4">
+                                    <CampaignOverview />
+                                    <CampaignOverview />
+                                </div>
+                                <div className="hr-manage-stat-container mt-4">
+                                    <div className="hr-manage-stat"></div>
+
+                                </div>
                             </div>
                         </div>
                         <div className="col-sm-1"></div>
                     </div>
                 </div>
-
             </div>
         );
     }
