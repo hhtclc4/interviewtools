37a053e0d4011abdca626cbaea97230b8c06d7b8
hoangclc4 <hoangclc4@gmail.com>
2020-06-27 19:46:38 +0700

    ??

--- FrontEnd/src/components/Admin/Page/Reports/ReportDetail/Player/Players.js
+++ FrontEnd/src/components/Admin/Page/Reports/ReportDetail/Player/Players.js
@@ -24,6 +24,11 @@ class ReportPlayers extends React.Component {
   render() {
     let { data, questions } = this.props;
     let { isShowQuiz, isOpenPopup } = this.state;
+    let { correctAnswer, inCorrectAnswer, unAttemptAnswer } = data;
+    let questionLength = questions.length;
+    let correctAccuracy = (data.correctAnswer / questionLength) * 100;
+    let inCorrectAccuracy = (data.inCorrectAnswer / questionLength) * 100;
+    let unAttemptAccuracy = (data.unAttemptAnswer / questionLength) * 100;
     return (
       <div
         className="report-detail-player-container container-fluid "
@@ -36,22 +41,35 @@ class ReportPlayers extends React.Component {
               src={require("../../../../../../utils/images/defaultava.png")}
             />
           </div>
-          <div className="rdp-name col-2 align-self-center">Player name{data.name}</div>
+          <div className="rdp-name col-2 align-self-center">
+            {data.user.name}
+          </div>
           {/*De so zo*/}
           <div className="rdp-accuracy-bar col-5 align-self-center">
             <div className="bar-display">
-              <div className="right-display"><span>5</span></div>
-              <div className="wrong-display"><span>4</span></div>
-              <div className="unattempt-display"><span>1</span></div>
+              <div
+                className="right-display"
+                style={{ width: `${correctAccuracy}%` }}
+              >
+                <span>{correctAnswer}</span>
+              </div>
+              <div
+                className="wrong-display"
+                style={{ width: `${inCorrectAccuracy}%` }}
+              >
+                <span>{inCorrectAnswer}</span>
+              </div>
+              <div
+                className="unattempt-display"
+                style={{ width: `${unAttemptAccuracy}%` }}
+              >
+                <span>{unAttemptAnswer}</span>
+              </div>
             </div>
           </div>
-          {/*De so zo*/}
           <div className="rdp-accuracy-rate col-3 align-self-center">
-            100%
+            {data.accuracy}%
           </div>
-          {/*De so zo*/}
-
-
         </div>
         {isShowQuiz ? (
           <PopUp
